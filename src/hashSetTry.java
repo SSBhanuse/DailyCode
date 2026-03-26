ec9bac8bb4f300a17ee3bcb8df98ca9f5fe8b1fb
SSBhanuse <sanketbhanuse3@gmail.com>
2026-03-26 14:15:55 +0530

    26-03-2026(2)

---
+++ src/hashSetTry.java
@@ -0,0 +1,27 @@
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashSet;
+
+public class hashSetTry {
+    public static void main(String[] args) {
+        //Capacity of HashSet is not fixed, it can grow dynamically as we add more elements to it, and it does not have a default capacity like ArrayList
+        HashSet <Integer> s1 = new HashSet<>();
+        s1.add(1);
+        s1.add(2);
+        s1.add(3);
+        s1.add(4);
+        s1.add(5);
+        s1.add(1); //HashSet does not allow duplicate elements, so this element will not be added to the set
+        System.out.println(s1); //HashSet does not maintain the order of the elements, so the output may not be in the same order as we added
+
+
+        Date date = new Date();
+        System.out.println(date);
+        System.out.println(date.getClass()); //getClass() method is used to get the class of the object, it will return the class of the object as a string
+        System.out.println(date.getTime()); //getTime() method is used to get the time in milliseconds since January 1, 1970, 00:00:00 GMT
+
+        Calendar cal = Calendar.getInstance(); //getInstance() method is used to get a calendar object that is initialized with the current date and time
+        System.out.println(cal);
+        System.out.println(cal.getTimeZone().getID());
+    }
+}
