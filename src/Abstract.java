fb81c2019a8ad402eb9aeca41575359f7552eecc
SSBhanuse <sanketbhanuse3@gmail.com>
2026-06-13 11:04:37 +0530

    13-06-2026(1)

--- src/Abstract.java
+++ src/Abstract.java
@@ -2,25 +2,35 @@
 
 abstract class Car2{
     public abstract void driveCar();
+    public abstract void ejectCar();
 
     public void playMusic(){
         System.out.println("Playing music in the car");
     }
 }
 
-class Creta extends Car2{
+abstract class Creta extends Car2{
     @Override
-    //If we're extending abstract class then need to define abstract method in that class
+    //If we're extending abstract class and in that class we have an abstract method then that abstract method need to define in that class
     public void driveCar() {
         System.out.println("Driving the Creta");
+    }
+}
+
+// Concrete class
+class Thar extends Creta{
+    @Override
+    public void ejectCar() {
+        System.out.println("Ejecting the Thar");
     }
 }
 
 public class Abstract {
     public static void main(String[] args) {
         //We cannot create aboject of abstract class
-        Car2 c2 = new Creta();
+        Car2 c2 = new Thar();
         c2.driveCar();
         c2.playMusic();
+        c2.ejectCar();
     }
 }
